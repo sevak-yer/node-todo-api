b7bacd3e3cab95241193ad3783ed19b6da0a6fc3
Sevak <yeressian@gmail.com>
2018-10-07 00:17:05 +0400

    mongodb-update.js added

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+//    db.collection('Todos').findOneAndUpdate({
+//        _id: new ObjectID('5bb799bb6d967b08ec122942')
+//    }, {
+//        $set: {
+//            completed: true
+//        }
+//    }, {
+//        returnOriginal: false
+//    }).then((result) => {
+//         console.log(result);
+//    });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5bb79b3c82d5e01e0c690da5')
+    },{
+        $set: {
+            name: 'Sevak'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then ((result) => {
+        console.log(result);
+    });
+
+    // db.close();
+});
+ 
