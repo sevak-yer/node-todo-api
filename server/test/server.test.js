ba2844113117adbf957c82a823f51fc4574c6380
Sevak <yeressian@gmail.com>
2018-10-18 12:29:46 +0400

    Test the DELETE /todos/:id route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,4 +1,4 @@
-const expect = require('expect');
+ const expect = require('expect');
 const request = require('supertest');
 const {ObjectID} = require('mongodb');
 
@@ -110,6 +110,52 @@ describe('GET /todos/:id', () => {
             .expect(404)
             .expect((res) => {
                 expect(res.text).toBe('invalid ID');
+            })
+            .end(done);
+    });
+});
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var id = todos[1]._id.toHexString();
+        request(app)
+            .delete(`/todos/${id}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(id)
+            })
+            
+            .end((err, res) => {
+                if (err) {
+                   return done(err);
+                }
+                Todo.findById(id).then((res) => {
+                    expect(res).toBeFalsy();
+                    done();
+                }).catch((e) => {
+                    done(e);
+                });
+                
+            });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var id = new ObjectID;
+        request(app) 
+            .delete(`/todos/${id.toHexString()}`)
+            .expect(404)
+            .expect((res) => {
+                expect(res.text).toBe('todo not found');
+            })
+            .end(done);
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+            .delete('/todos/123')
+            .expect(404)
+            .expect((res) => {
+                expect(res.text).toBe('Invalid ID');
             })
             .end(done);
     });
